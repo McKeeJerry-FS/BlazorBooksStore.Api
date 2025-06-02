e185dc88da71e5cd1493db876fd8e53c236386fe
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2025-06-02 17:58:19 -0400

    Fixed the API to run correctly

---
+++ API/BooksStore.Api/BooksStore.Api/Services/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,9 @@
+﻿using BooksStore.Api.Models;
+
+namespace BooksStore.Api.Services.Interfaces
+{
+    public interface IAuthenticationService
+    {
+        Task<LoginResponse> LoginAsync(LoginRequest model);
+    }
+}
